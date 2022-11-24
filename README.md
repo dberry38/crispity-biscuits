7a35f5deba55c28a025e62a2b9b963e81072be50
dberry38 <108836058+dberry38@users.noreply.github.com>
2022-11-23 17:40:31 -0800

    Develop Models and associations

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
+<!-- TODO finish -->
+
 # crispity-biscuits
+
+## Description
+A blog site for sharing tips and articles related to tech.
+
+## Motivation
+Creating a simple interface for keeping notes.
+
+## Installation
+N/A
+
+## Usage
+Click get started to take notes except it's not working.
+
+## Demonstration
+N/A
+
+## Credits
+N/A
+
+## License
+Refer to MIT lisence in GitHub repo
---
+++ controllers/home-routes.js
@@ -0,0 +1,63 @@
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models");
+
+// WHEN I visit the site for the first time
+// THEN I am presented with the homepage, which includes existing blog posts if any have been posted;
+router.get("/", (req, res) => {
+  Post.findAll({
+    include: [User],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+      res.render("all-posts", { posts });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+
+router.get("/post/:id", (req, res) => {
+  Post.findByPk(req.params.id, {
+    include: [
+      User,
+      {
+        model: Comment,
+        include: [User],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+
+        res.render("single-post", { post });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
+
+module.exports = router;
